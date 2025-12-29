c7909413ad7f6c3af449d6d1627f72458bd24dff
arvchahal <arnavchahal14@gmail.com>
2025-12-28 20:40:25 -0500

    Revert "save before i do some breaking changes"
    This reverts commit f4c5af3147c106bacdbd194c18e912e0c89b526e.

--- include/vlite-ml/tensor.h
+++ include/vlite-ml/tensor.h
@@ -4,16 +4,5 @@
 
 #ifndef TENSOR_H
 #define TENSOR_H
-#include <torch/torch.h>
-#include <vlite-sampling/sampler.h>
-#include <vector>
-#include "../vlite-core/frame.h"
-
-namespace vlite {
-        torch::Tensor clips_to_tensors(const std::vector<std::shared_ptr<Clip>> & clips);
-        torch::Tensor frame_to_tensor(const Frame *frame);
-        torch::Tensor clip_to_tensors(const Clip *clip);
-
-}
 
 #endif //TENSOR_H
--- include/vlite-sampling/sampler.h
+++ include/vlite-sampling/sampler.h
@@ -13,7 +13,6 @@ struct Clip {
 public:
   std::vector<std::shared_ptr<Frame>> frames;
 };
-
 inline std::ostream &operator<<(std::ostream &os, const Clip &c) {
   std::cout << "Frames in this clip" << &c.frames << std::endl;
   return os;
